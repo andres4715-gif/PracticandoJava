8a9b897855f0009ff7efb3298ca6abbecea84c61
Andres Rios <69942140+andres4715-gif@users.noreply.github.com>
2022-06-23 09:38:16 -0500

    new code added

---
+++ src/ejerciciosJava/practicando/EstructuraCondicional/Ejercicio1.java
@@ -0,0 +1,11 @@
+package ejerciciosJava.practicando.EstructuraCondicional;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+
+        // http://puntocomnoesunlenguaje.blogspot.com/2012/07/java-ejercicios-basicos-condicional-1.html
+        // Programa Java que lea un número entero por teclado y calcule si es par o impar.
+
+
+    }
+}
