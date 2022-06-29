ebf13786d16332607e72292160212de2abf12dfa
Andres Rios <69942140+andres4715-gif@users.noreply.github.com>
2022-06-29 05:21:15 -0500

    adding array to lists

---
+++ src/ejerciciosJava/practicando/arrays/Data.java
@@ -0,0 +1,29 @@
+package ejerciciosJava.practicando.arrays;
+
+public class Data {
+
+    public void sumaAndmedia(int[] A) {
+        double sumaPos = 0;
+        double sumaNeg = 0;
+        int pos = 0;
+        int neg = 0;
+
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] >= 0) {
+                sumaPos += A[i];
+                pos++;
+            } else {
+                sumaNeg += A[i];
+                neg++;
+            }
+        }
+
+        System.out.println("Positivos son: " + sumaPos);
+        System.out.println("Negativos son: " + sumaNeg);
+        System.out.println("Cantidad positivos son: " + pos);
+        System.out.println("Cantidad negativos son: " + neg);
+
+        System.out.println("Media positivos es: " + (sumaPos / pos));
+        System.out.println("media negativos es: " + (sumaNeg / neg));
+    }
+}
--- src/ejerciciosJava/practicando/arrays/Ejercicio1.java
+++ src/ejerciciosJava/practicando/arrays/Ejercicio1.java
@@ -8,31 +8,9 @@ public class Ejercicio1 {
      */
 
     public static void main(String[] args) {
-        Ejercicio1.sumaAndmedia(new int [] {1, 2, 3, 4, 5, -1, -2, -3, -4, -5});
-    }
 
-    public static void sumaAndmedia(int[] A) {
-        double sumaPos = 0;
-        double sumaNeg = 0;
-        int pos = 0;
-        int neg = 0;
-
-        for (int i = 0; i < A.length; i++) {
-            if (A[i] >= 0) {
-                sumaPos += A[i];
-                pos++;
-            } else {
-                sumaNeg += A[i];
-                neg++;
-            }
-        }
-
-        System.out.println("Positivos son: " + sumaPos);
-        System.out.println("Negativos son: " + sumaNeg);
-        System.out.println("Cantidad positivos son: " + pos);
-        System.out.println("Cantidad negativos son: " + neg);
-
-        System.out.println("Media positivos es: " + (sumaPos / pos));
-        System.out.println("media negativos es: " + (sumaNeg / neg));
+        Data hs = new Data();
+        hs.sumaAndmedia(new int[]{1, 2, 3, 4, 5, -1, -2, -3, -4, -5});
     }
 }
+
