83fe7646ababc247257041e925da323806eb0b8b
Andres Rios <69942140+andres4715-gif@users.noreply.github.com>
2022-06-29 07:39:57 -0500

    adding array to lists

--- src/ejerciciosJava/practicando/arrays/Data.java
+++ src/ejerciciosJava/practicando/arrays/Data.java
@@ -2,17 +2,17 @@ package ejerciciosJava.practicando.arrays;
 
 public class Data {
 
-    public static void sumaAndmedia(int[] A) {
+    public static void sumaAndMedia(int[] A) {
         double sumaPos = 0;
         double sumaNeg = 0;
         int pos = 0;
         int neg = 0;
 
         for (int i = 0; i < A.length; i++) {
-            if (A[i] >= 0) {
+            if (A[i] >= 1 ) {
                 sumaPos += A[i];
                 pos++;
-            } else {
+            } else if (A[i] < 0 ){
                 sumaNeg += A[i];
                 neg++;
             }
@@ -25,5 +25,15 @@ public class Data {
 
         System.out.println("Media positivos es: " + (sumaPos / pos));
         System.out.println("media negativos es: " + (sumaNeg / neg));
+    }
+
+    public static int[] data() {
+        int contador = -5;
+        int[] todaLaData = new int[11];
+        for (int i = 0; i < todaLaData.length; i++) {
+            todaLaData[i] = contador;
+            contador++;
+        }
+        return todaLaData;
     }
 }
---
+++ src/ejerciciosJava/practicando/arrays/Ejercicio2.java
@@ -0,0 +1,12 @@
+package ejerciciosJava.practicando.arrays;
+
+public class Ejercicio2 extends Data {
+
+    /*
+        Programa Java que lea 10 números enteros por teclado y los guarde en un array. Calcula y muestra la media de los números que estén en las posiciones pares del array.
+        Considera la primera posición del array (posición 0) como par.
+        // http://puntocomnoesunlenguaje.blogspot.com/2012/07/arreglos-en-java-calcular-media.html
+     */
+
+}
+
