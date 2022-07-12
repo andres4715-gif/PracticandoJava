323010d37bbc25f068177a05cbeb4151de836221
Andres Rios <69942140+andres4715-gif@users.noreply.github.com>
2022-07-11 20:22:10 -0500

    new challenge

---
+++ src/ejerciciosJava/practicando/ArrayList/ejercicio8/Coche.java
@@ -0,0 +1,28 @@
+package ejerciciosJava.practicando.ArrayList.ejercicio8;
+
+public class Coche {
+
+    public Coche(String marca, double kilometros) {
+        this.marca = marca;
+        this.kilometros = kilometros;
+    }
+
+    private String marca;
+    private double kilometros;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String setMarca(String marca) {
+        return marca;
+    }
+
+    public double getKilometros() {
+        return kilometros;
+    }
+
+    public double setKilometros(double kilometros) {
+        return kilometros;
+    }
+}
