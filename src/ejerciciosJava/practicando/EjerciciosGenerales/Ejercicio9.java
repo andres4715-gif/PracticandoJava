9db9e92769dd8e6d42ac3fef47e3cfb9d258c390
Andres Rios <69942140+andres4715-gif@users.noreply.github.com>
2022-07-14 09:27:17 -0500

    ejercicio 4 ready

---
+++ src/ejerciciosJava/practicando/EjerciciosGenerales/Ejercicio9.java
@@ -0,0 +1,19 @@
+package ejerciciosJava.practicando.EjerciciosGenerales;
+
+import java.util.ArrayList;
+
+public class Ejercicio9 {
+
+    /*
+            Progama que indique el total a pagar en una farmacia si el total apagar es:
+             0 a 500  0% de descuento
+             501 a 1000 5%
+             1001 a 7000 11%
+             7001 a 15000 18%
+             150001 en adelante 25%
+    */
+
+    public static void main(String[] args) {
+
+    }
+}
