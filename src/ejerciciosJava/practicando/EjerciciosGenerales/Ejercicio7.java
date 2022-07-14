fccf1b263b10d547686ab93ab7e3ab9caac01ddc
Andres Rios <69942140+andres4715-gif@users.noreply.github.com>
2022-07-14 08:28:56 -0500

    ejercicio 4 ready

---
+++ src/ejerciciosJava/practicando/EjerciciosGenerales/Ejercicio7.java
@@ -0,0 +1,12 @@
+package ejerciciosJava.practicando.EjerciciosGenerales;
+
+public class Ejercicio7 {
+
+    /*
+         PROGRAMA QUE IMPRIMA LAS 10 TABLAS DE MULTIPLICAR
+    */
+
+    public static void main(String[] args) {
+
+    }
+}
