8a76339bf06d34e445d1c9ce6bfa9f186aaf9d70
Andres Rios <69942140+andres4715-gif@users.noreply.github.com>
2022-07-14 15:00:25 -0500

    ejercicio 4 ready

---
+++ src/ejerciciosJava/practicando/recursividad/Ejercicio1.java
@@ -0,0 +1,15 @@
+package ejerciciosJava.practicando.recursividad;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        hola(1);
+    }
+
+    public static void hola(int contador) {
+        if (contador <= 5) {
+            System.out.print(contador + " ");
+            hola(contador + 1);
+        }
+    }
+}
