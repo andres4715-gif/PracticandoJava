38d1231b9956d5802415db7313dc09ae17b3307d
arios <andres.rios@ctg.com>
2023-01-30 10:52:04 -0500

    adding examples to get information from a properties files

---
+++ src/ejerciciosJava/MyPractice/codility/agileEngine/Test.java
@@ -0,0 +1,10 @@
+package ejerciciosJava.MyPractice.codility.agileEngine;
+
+public class Test {
+
+    public static void main(String[] args) {
+        int numero = 34;
+        System.out.println("El numero es: " + numero);
+        System.out.println("El numero es: " + String.valueOf(numero).charAt(0));
+    }
+}
