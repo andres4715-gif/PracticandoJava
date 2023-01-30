56d8cde138a10bc07c03500ee7fbc7f8f241655a
arios <andres.rios@ctg.com>
2023-01-30 18:31:44 -0500

    new data

---
+++ src/ejerciciosJava/MyPractice/practicing/enums/CallEnumValues.java
@@ -0,0 +1,9 @@
+package ejerciciosJava.MyPractice.practicing.enums;
+
+public class CallEnumValues {
+
+    public static void main(String[] args) {
+        dayOfWeek today = dayOfWeek.FRIDAY;
+        System.out.println(today);
+    }
+}
---
+++ src/ejerciciosJava/MyPractice/practicing/enums/dayOfWeek.java
@@ -0,0 +1,11 @@
+package ejerciciosJava.MyPractice.practicing.enums;
+
+public enum dayOfWeek {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
