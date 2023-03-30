a4052a891f8611170ff8a9d94325037472c5078e
arios <andres.rios@ctg.com>
2023-03-29 20:20:24 -0500

    new data

--- src/ejerciciosJava/MyPractice/entrevistas/ejerciciosDePractica/CalculandoValores.java
+++ src/ejerciciosJava/MyPractice/entrevistas/ejerciciosDePractica/CalculandoValores.java
@@ -7,12 +7,13 @@ import java.util.List;
 
 public class CalculandoValores {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1,3,5,7,9);
+        List<Integer> numbers = Arrays.asList(1,5,3,7,9);
 
         long sumatoriaChicos =  0;
         long sumatoriaGrandes = 0;
 
         Collections.sort(numbers);
+        System.out.println("--- After sort: " + numbers);
 
         for(int i = 0; i < numbers.size()-1 ; i++) {
             sumatoriaChicos += numbers.get(i);
