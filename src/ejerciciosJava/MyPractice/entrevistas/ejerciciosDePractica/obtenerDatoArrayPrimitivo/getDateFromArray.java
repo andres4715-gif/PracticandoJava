c8c950565ea1b9d4cd9a8fb710e3f8294ab2bb8e
Andres Rios <andres.rios@intelex.com>
2025-01-18 11:44:19 -0500

    new data added

---
+++ src/ejerciciosJava/MyPractice/entrevistas/ejerciciosDePractica/obtenerDatoArrayPrimitivo/getDateFromArray.java
@@ -0,0 +1,13 @@
+package ejerciciosJava.MyPractice.entrevistas.ejerciciosDePractica.obtenerDatoArrayPrimitivo;
+
+public class getDateFromArray {
+    // Obtener n data de un array primitivo
+    public static void main(String[] args) {
+        Operation operation = new Operation();
+
+        int[] array = {1, 2, 3, 4, 5};
+
+        int value = Operation.getValueFromPrimitiveArray(operation.finalData(array), 1);
+        System.out.println(value);
+    }
+}
