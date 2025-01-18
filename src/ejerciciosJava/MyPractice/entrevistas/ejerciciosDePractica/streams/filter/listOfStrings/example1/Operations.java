d6acbef5c8667781566cda3f085210466c7882bf
Andres Rios <andres.rios@intelex.com>
2025-01-18 15:25:22 -0500

    COMMIT MESSAGE

---
+++ src/ejerciciosJava/MyPractice/entrevistas/ejerciciosDePractica/streams/filter/listOfStrings/example1/Operations.java
@@ -0,0 +1,13 @@
+package ejerciciosJava.MyPractice.entrevistas.ejerciciosDePractica.streams.filter.listOfStrings.example1;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Operations {
+
+    public static List<String> myData(List<String> data, String word) {
+        return data.stream()
+                .filter(name -> name.contains(word))
+                .collect(Collectors.toList());
+    }
+}
