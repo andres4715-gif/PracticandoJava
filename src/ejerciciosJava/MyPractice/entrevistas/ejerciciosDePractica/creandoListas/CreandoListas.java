21743752c16508119da8e9aa115596dadc9d1ce2
Andres Rios <andres.rios@intelex.com>
2025-01-18 11:04:51 -0500

    new data added

---
+++ src/ejerciciosJava/MyPractice/entrevistas/ejerciciosDePractica/creandoListas/CreandoListas.java
@@ -0,0 +1,17 @@
+package ejerciciosJava.MyPractice.entrevistas.ejerciciosDePractica.creandoListas;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CreandoListas {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        List<Integer> myFinalData = ArrayToList.arrayToList(numbers);
+        List<Integer> newData = myFinalData.stream()
+                .filter(x -> x > 4)
+                .map(x -> x + 200)
+                .collect(Collectors.toList());
+        System.out.println(newData);
+    }
+}
