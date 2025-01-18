ae7ef0378c922650851cc01774c9de287d86f2cd
Andres Rios <andres.rios@intelex.com>
2025-01-18 12:24:01 -0500

    new data added

---
+++ src/ejerciciosJava/MyPractice/entrevistas/ejerciciosDePractica/sumasAcomuladas/Operation.java
@@ -0,0 +1,13 @@
+package ejerciciosJava.MyPractice.entrevistas.ejerciciosDePractica.sumasAcomuladas;
+
+public class Operation {
+
+    protected int[] cumulativeSums(int[] nums) {
+        int[] result = new int[nums.length];
+        result[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            result[i] = result[i - 1] + nums[i];
+        }
+        return result;
+    }
+}
