ce58c8071c13ee53a7922d567761de8c392476ff
Andres Rios <andres.rios@intelex.com>
2025-01-18 09:34:07 -0500

    new data

--- src/ejerciciosJava/MyPractice/codility/Doing.java
+++ src/ejerciciosJava/MyPractice/codility/Doing.java
@@ -5,20 +5,12 @@ import static ejerciciosJava.MyPractice.codility.Solution.solution;
 public class Doing {
 
     /*
-
-
 Write a function:
-
 class Solution { public int solution(int[] A); }
-
 that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
-
 For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
-
 Given A = [1, 2, 3], the function should return 4.
-
 Given A = [−1, −3], the function should return 1.
-
 Write an efficient algorithm for the following assumptions:
 
 N is an integer within the range [1..100,000];
@@ -32,7 +24,7 @@ Copyright 2009–2023 by Codility Limited. All Rights Reserved. Unauthorized cop
         // int[] A = {1, 3, 6, 4, 1, 2, 7, 8, 9};
         // int[] A = {-1, -3};
         // int[] A = {1, 2, 3};
-        int[] A = {1, 2, 3, 5, 6};
-        solution(A);
+        int[] data = {1, 2, 3, 5, 6};
+        solution(data);
     }
 }
--- src/ejerciciosJava/MyPractice/codility/Solution.java
+++ src/ejerciciosJava/MyPractice/codility/Solution.java
@@ -4,10 +4,10 @@ import java.util.HashSet;
 
 public class Solution {
 
-    public static int solution(int[] A) {
+    public static int solution(int[] data) {
         HashSet<Integer> hashSet = new HashSet<>();
         int min = 1;
-        for (int i : A) {
+        for (int i : data) {
             if (i > 0) {
                 hashSet.add(i);
             }
