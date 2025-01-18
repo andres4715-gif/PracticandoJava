da49854776df1bafcbdcb7409b5a6c71bd09662e
Andres Rios <andres.rios@intelex.com>
2025-01-18 09:26:04 -0500

    new data

--- src/ejerciciosJava/MyPractice/codility/agileEngine/Hashset.java
+++ src/ejerciciosJava/MyPractice/codility/agileEngine/Hashset.java
@@ -5,14 +5,12 @@ import java.util.HashSet;
 public class Hashset {
 
     // Lo primero que hay que saber es que hashset no es un array es una lista
+    // El tema con el hashSet es que no permite datos repetidos
 
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5, 6, 6, 6, 2};
 
         HashSet<Integer> hashSetValues = new HashSet<>();
-        HashSet<Integer> newHashSetData = new HashSet<Integer>();
-        System.out.println(newHashSetData);
-
         for (int data : numbers) {
             if (data > 5) {
                 hashSetValues.add(data);
