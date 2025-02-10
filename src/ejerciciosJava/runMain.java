e6efd2261f625ca4ecf5c2475aa5ee10634c691a
Andres Rios <andres.rios@intelex.com>
2025-02-10 17:56:35 -0500

    new array exercice ready

--- src/ejerciciosJava/runMain.java
+++ src/ejerciciosJava/runMain.java
@@ -1,7 +1,10 @@
 package ejerciciosJava;
 
+import java.util.function.Function;
+
 public class runMain {
     public static void main(String[] args) {
-
+        Function<Integer, Integer> cuadrado = x -> x * x;
+        System.out.println(cuadrado.apply(5));  // Salida: 25
     }
 }
